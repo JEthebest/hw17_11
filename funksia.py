70d645f3c7bb9af87f1fd25a0944ab13e58e7eaa
JEthebest <iskokairatov@gmail.com>
2022-11-18 19:04:03 +0600

    first commit

---
+++ funksia.py
@@ -0,0 +1,21 @@
+def show_even_numbers(*args):
+    even_numbers_lst = []
+    for i in args:
+        try:
+            if i % 2 == 0:
+                even_numbers_lst.append(i)
+
+        except TypeError:
+            continue 
+        
+    return even_numbers_lst
+
+
+def vyvod(even_numbers_lst):
+    count = 1
+    for i in even_numbers_lst:
+        print(f'{count} - {i}')
+        count += 1
+
+vyvod(show_even_numbers(3, 8, 'hello', 100, [14, 13, 12], 12))
+
